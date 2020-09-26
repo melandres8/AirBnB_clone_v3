81d4ea0a962a611d9fb9b2f92706639062cfe70d
Melkin Mosquera <melanmosta@gmail.com>
2020-09-26 17:25:17 -0500

    task#3: Verifying the API status before requesting

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  const amenities = {};
+  const h4Amenity = $('.amenities h4');
+  const checkbox = $('.amenities input');
+  checkbox.css('margin-right', '10px');
+  // this doesn't work with arrow functions
+  $('li input[type="checkbox"]').change(function () {
+    if ($(this).prop('checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+        delete amenities[$(this).data('id')];
+    }
+    h4Amenity.text(Object.values(amenities).join(', '));
+  });
+});
+
+/* API status */
+$.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+  if ($(data.status === 'OK')) {
+    $('DIV#api_status').addClass('available');
+    $('DIV#api_status.available').css({ 'background-color': '#ff545f' });
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
